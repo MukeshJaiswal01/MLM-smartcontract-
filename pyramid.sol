f9fca3b1954b04500dfad9b5a8d2c650ee1e67f8
Mukesh Jaiswal <56516302+mukesh0000@users.noreply.github.com>
2020-08-16 22:31:47 +0530

    Create pyramid.sol

---
+++ pyramid.sol
@@ -0,0 +1,53 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+
+contract simplePyramid{
+    
+    uint public constant MinimumInvestmen = 1e15;
+    uint public numInvestor = 0;
+    uint public depth = 0;
+     uint investors_length = 3;
+    address payable [] public investors;
+    mapping (address => uint) public balances;
+    
+    constructor ()  payable public{
+        require(msg.value >= MinimumInvestmen, "less money");
+        
+        investors.push(msg.sender);
+        numInvestor = 1;
+        depth = 1;
+        balances[address(this)] = msg.value;
+        
+        
+    }
+    function () payable external{
+        require(msg.value >= MinimumInvestmen, "less money");
+        balances[address(this)] += msg.value;
+        numInvestor += 1;
+        investors[numInvestor - 1] = msg.sender;
+        
+        if(numInvestor == investors_length){
+            uint endIndex = numInvestor - 2**depth;
+            uint startIndex = endIndex - 2**(depth-1);
+            
+            for(uint i = startIndex; i < endIndex; i++){
+                balances[investors[i]] += MinimumInvestmen;
+            }
+            uint paid = MinimumInvestmen * 2**(depth-1);
+            uint eachInvestment= (balances[address(this)] - paid) / numInvestor;
+            for(uint i = 0; i < numInvestor; i++){
+                balances[investors[i]] += eachInvestment;
+            }
+            balances[address(this)] = 0;
+            depth += 1;
+            investors_length += 2**depth;
+            
+        }
+    }
+    
+    function withdraw() public{
+        uint payout = balances[msg.sender];
+        balances[msg.sender] = 0;
+        msg.sender.transfer(payout);
+    }
+}
